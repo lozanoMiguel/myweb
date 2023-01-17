4547e0aebe8ef9bb59101c2ccc39db06ddf81e29
lozanoMiguel <lozanomiguel92@gmail.com>
2023-01-17 16:03:00 +0100

    modify profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -11,11 +11,9 @@ function Profile(){
                 <section className="about">
                     <h2>Hola, me llamo <span style={{color: '#398B82'}}>Miguel</span> y ésta es mi <span style={{color:'#E78B0F'}}>web</span></h2>
                     <p>He creado este sitio web a modo de portfolio para  mostrar mis trabajos y brindar algo de información sobre mi.</p>
-                    <p>Soy un desarrollador junior que está buscando su primer oportunidad laboral en el campo de la informática.</p>
-                    <p>Si bien me inclino por el desarrollo frontend, el backend me genera mucho interés. Soy de pensar que el conocimiento 
-                       sobre algún lenguaje es igual de equiparable a saber identificar que es lo que no te deja avanzar en la elaboracion
-                       de un proyecto. 
-                    </p>
+                    <p>Volcado hacia el Frontend pero con conocimientos en Backend. En busca de iniciar mi carrera profesional en el campo del desarrollo.</p>
+                    <p>Me gusta pensar que saber buscar lo que no te deja avanzar es una herramienta muy poderosa tanto para el desarrollo como para la vida.</p>
+                    <p>Te animo a contactarme, más abajo encontrarás cómo!</p>
                     <p>En mis ratos libres juego al fútbol, toco la guitarra y disfruto el tiempo con mis amigos y amigas.</p>
                        
                 </section>
